d47d1f8e23a5d5ef6e5763d2052952db8aeb637c
Eles-080 <155256138+Eles-080@users.noreply.github.com>
2024-01-21 13:54:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    /*    5 + 7 + 8 = 20
+        5 * 7 * 8 = 280
+        (5 + 7 + 8) / 3 = 6.6666666667*
+
+     */
+        byte a = 5;
+        byte b = 7;
+        byte c = 8;
+        int summa = a + b + c;
+        int ymnojenya =a * b * c;
+        double delenya = (a+b+c)/3.0;
+        System.out.println("5+7+8=" +summa);
+        System.out.println("5*7*8=" +ymnojenya);
+        System.out.println("(5+7+8)/3=" +delenya);
+
+    }
+}
